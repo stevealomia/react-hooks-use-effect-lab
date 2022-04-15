6f85aba7aadd354b0eb105edbc2a19ccc6c85ad0
Steve Alomia <stevealomia@yahoo.com>
2022-04-15 09:47:04 -0400

    commit tree e02eba6714336018cb424e04467ce59d188155c9

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  /* const timeRef = useRef(timeRemaining);
+  timeRef.current = timeRemaining; */
 
   // add useEffect code
+ useEffect (() => {
+   setTimeout(() => {
+     return timeRemaining -1
+   })
+
+ },1000)
+
+	//When timeRemaining hits 0, reset timeRemaining back to 10 seconds, and call the onAnswered callback prop with a value of false to
+	//trigger a behavior on the app component
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
